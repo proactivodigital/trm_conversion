5b30ce4b2ae280a9a874be4ee372e8b57c0dbf96
Cristian Berrio <Cristian.Berrio@proactivo.digital>
2024-02-05 11:29:30 -0500

    test

--- trm_account/models/trm_subscription.py
+++ trm_account/models/trm_subscription.py
@@ -1,12 +1,19 @@
-# Importa las clases necesarias desde el módulo 'odoo'
 from odoo import models, fields, api
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+from datetime import datetime
 
-# Define tu modelo 'Trm' que hereda del modelo 'account.move'
 class TrmSubscription(models.Model):
     _inherit = 'sale.order'
 
-    # Definición de campos adicionales
     trm_date = fields.Date('TRM Date', default=fields.Date.today(), readonly=True, states={'draft': [('readonly', False)]})
     trm_value = fields.Float('TRM Value', readonly=True, compute='_compute_trm_value', store=True)
     currency_id = fields.Many2one('res.currency', string='Main Currency', domain="[('active', '=', True)]")
-    
+
+    @api.depends('currency_id', 'trm_date', 'amount_total')
+    def _compute_trm_value(self):
+        for order in self:
+            # Obtén la tasa de cambio para la moneda seleccionada en la fecha actual
+            rate = order.currency_id._get_conversion_rate(order.trm_date, company_id=order.company_id.id)
+
+            # Calcula el valor de TRM multiplicando la tasa de cambio por la cantidad deseada
+            order.trm_value = rate * order.amount_total
